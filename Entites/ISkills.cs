17a98782eafbec3d2208e4d669c629f561b659bb
Aposrgb <63498564+Aposrgb@users.noreply.github.com>
2021-07-09 00:19:12 +0400

    Add files via upload

---
+++ Entites/ISkills.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    public interface ISkills
+    {
+        List<string> GetListTypeSkills();
+        string AddSkill(string name, string type);
+        void DeleteSkill(string name, string type, int Id);
+        List<Skills> GetListSkills();
+    }
+}
---
+++ Entites/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    public abstract class IUser
+    {
+        public int session_id;
+        public abstract string EntryAccount(string Login, string Password);
+        public abstract List<Skills> GetSkillsUser(int Id);
+        public abstract string GetUserLogin(int Id);
+        public abstract void AddSkillUser(string name, string type, int Id);
+        public abstract string CreateUser(string Email, string Login, string Password);
+        public abstract string UpdateLogin(int Id, string Login);
+    }
+}
