c37bc8b2aabe0446825971b6b5c22845b9ece017
Aposrgb <63498564+Aposrgb@users.noreply.github.com>
2021-07-09 16:24:58 +0400

    Add files via upload

---
+++ Dependecy/DependecyResolver.cs
@@ -0,0 +1,23 @@
+﻿using BLInterfaces;
+using BussinessLayer;
+using DataLayer;
+using Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependecy
+{
+    public class DependecyResolver
+    {
+        #region SINGLTONE
+        private static DependecyResolver _instance;
+        public static DependecyResolver Instance => _instance = new DependecyResolver();
+        #endregion;
+
+        public IBL b = new BL();
+        static void Main(string[] args){}
+    }
+}
