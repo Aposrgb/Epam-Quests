15f2ef024b9272d8f73fa5d22431ef58aaca3784
Aposrgb <63498564+Aposrgb@users.noreply.github.com>
2021-07-09 16:26:05 +0400

    Add files via upload

---
+++ BLInterfaces/IBL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLInterfaces
+{
+    public interface IBL
+    {
+        string CreateUser(string Email, string Login, string Password);
+        string EntryAccount(string Login, string Password);
+        string UpdateLogin(int Id, string Login);
+        string GetUserLogin(int Id);
+        List<ISkills> GetSkillsUser(int Id);
+        List<string> GetListTypeSkills();
+        List<ISkills> GetListSkills();
+        void DeleteSkill(string name, string type, int Id);
+        string AddSkill(string name, string type);
+        void AddSkillUser(string name, string type, int Id);
+        string HtmlListSkill(int Id);
+    }
+}
---
+++ BLInterfaces/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BLInterfaces
+{
+    public interface IUser
+    {
+        string EntryAccount(string Login, string Password);
+        List<ISkills> GetSkillsUser(int Id);
+        string GetUserLogin(int Id);
+        void AddSkillUser(string name, string type, int Id);
+        string CreateUser(string Email, string Login, string Password);
+        string UpdateLogin(int Id, string Login);
+    }
+}
