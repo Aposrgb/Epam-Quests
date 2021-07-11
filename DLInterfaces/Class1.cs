86a3b30bc9312d362ea7a80338f70e875ea283a5
Aposrgb <63498564+Aposrgb@users.noreply.github.com>
2021-07-11 23:40:41 +0400

    Add files via upload

---
+++ DLInterfaces/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLInterfaces
+{
+    class Class1
+    {
+        public static void Main() { }
+    }
+}
---
+++ DLInterfaces/IDL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public interface IDL
+    {
+        DataTable GetTypeSkill();
+        DataTable GetTypeSkill(int Id);
+        DataTable GetSkills(int Id);
+        DataTable GetSkills();
+        DataTable GetSkill_Id();
+        DataTable GetProfiles();
+        void AddSkill(string name, string type);
+        void AddSkill(string name, int Id);
+        void UpdateLoginUser(int Id, string Login);
+        void UserInsert(string Email, string Login, string Password);
+        void InsertSkillUser(int Id_skill, int Id_user);
+        void DeleteSkillUser(int Id_skill, int Id_user);
+    }
+}
