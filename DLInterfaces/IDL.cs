e14ece89dd7c672e88a8b99c053a974d018746cd
Aposrgb <63498564+Aposrgb@users.noreply.github.com>
2021-07-13 22:40:22 +0400

    Add files via upload

--- DLInterfaces/IDL.cs
+++ DLInterfaces/IDL.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using BLInterfaces;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataLayer
 {
@@ -15,11 +12,27 @@ namespace DataLayer
         DataTable GetSkills();
         DataTable GetSkill_Id();
         DataTable GetProfiles();
-        void AddSkill(string name, string type);
+        string AddSkill(string name, string type);
         void AddSkill(string name, int Id);
         void UpdateLoginUser(int Id, string Login);
         void UserInsert(string Email, string Login, string Password);
         void InsertSkillUser(int Id_skill, int Id_user);
         void DeleteSkillUser(int Id_skill, int Id_user);
+        List<ISkills_Logic> GetListSkills();
+        void DeleteSkill(string name, string type, int Id);
+        List<string> GetListTypeSkills();
+        string EntryAccount(string Login, string Password);
+        List<ISkills_Logic> GetSkillsUser(int Id);
+        string GetUserLogin(int Id);
+        void AddSkillUser(string name, string type, int Id);
+        string CreateUser(string Email, string Login, string Password);
+        string UpdateLogin(int Id, string Login);
+        string HtmlListSkill(int Id);
+        string GetStatus(int Id);
+        string SetStatus(int Id,string text);
+        string ChangePass(string Login, string old_pass, string new_pass);
+        string GetRole(int Id);
+        string EditHtmlListSkill(int Id);
+        string DeleteAllSkill(string name, string type);
     }
 }
