592ea85b49d93fde47901ec32e43b228421ba04b
Aposrgb <63498564+Aposrgb@users.noreply.github.com>
2021-07-16 15:06:55 +0400

    Add files via upload

--- BLInterfaces/IBL.cs
+++ BLInterfaces/IBL.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Entites;
 using System.Collections.Generic;
 
 namespace BLInterfaces
@@ -9,18 +9,17 @@ namespace BLInterfaces
         string EntryAccount(string Login, string Password);
         string UpdateLogin(int Id, string Login);
         string GetUserLogin(int Id);
-        List<ISkills_Logic> GetSkillsUser(int Id);
+        List<Skills> GetSkillsUser(int Id);
         List<string> GetListTypeSkills();
-        List<ISkills_Logic> GetListSkills();
+        List<Skills> GetListSkills();
         void DeleteSkill(string name, string type, int Id);
         string AddSkill(string name, string type);
         void AddSkillUser(string name, string type, int Id);
-        string HtmlListSkill(int Id);
         string GetStatus(int Id);
         string SetStatus(int Id,string text);
         string ChangePass(string Login,string old_pass,string new_pass, string check_pass);
         string GetRole(int Id);
-        string EditHtmlListSkill(int Id);
         string DeleteAllSkill(string name, string type);
+        void UpdateSkillName(string new_name,string name, string type);
     }
 }
