b304c220bcfbda3bcdf8ace9758d23d9738a0739
Aposrgb <63498564+Aposrgb@users.noreply.github.com>
2021-07-16 14:56:18 +0400

    Add files via upload

--- DLInterfaces/IDL.cs
+++ DLInterfaces/IDL.cs
@@ -1,4 +1,4 @@
-﻿using BLInterfaces;
+﻿
 using System.Collections.Generic;
 using System.Data;
 
@@ -7,32 +7,31 @@ namespace DataLayer
     public interface IDL
     {
         DataTable GetTypeSkill();
-        DataTable GetTypeSkill(int Id);
-        DataTable GetSkills(int Id);
+        DataTable GetTypeSkillonID(int Id);
+        DataTable GetSkillsonID(int Id);
         DataTable GetSkills();
-        DataTable GetSkill_Id();
+        DataTable GetSkillWithId();
         DataTable GetProfiles();
-        string AddSkill(string name, string type);
-        void AddSkill(string name, int Id);
+        string AddSkillOnType(string name, string type);
+        void AddSkillOnID(string name, int Id);
         void UpdateLoginUser(int Id, string Login);
         void UserInsert(string Email, string Login, string Password);
         void InsertSkillUser(int Id_skill, int Id_user);
         void DeleteSkillUser(int Id_skill, int Id_user);
-        List<ISkills_Logic> GetListSkills();
+        List<Entites.Skills> GetListSkills();
         void DeleteSkill(string name, string type, int Id);
         List<string> GetListTypeSkills();
         string EntryAccount(string Login, string Password);
-        List<ISkills_Logic> GetSkillsUser(int Id);
+        List<Entites.Skills> GetSkillsUser(int Id);
         string GetUserLogin(int Id);
         void AddSkillUser(string name, string type, int Id);
         string CreateUser(string Email, string Login, string Password);
         string UpdateLogin(int Id, string Login);
-        string HtmlListSkill(int Id);
         string GetStatus(int Id);
         string SetStatus(int Id,string text);
         string ChangePass(string Login, string old_pass, string new_pass);
         string GetRole(int Id);
-        string EditHtmlListSkill(int Id);
         string DeleteAllSkill(string name, string type);
+        void UpdateSkillName(string new_name,string name, string type);
     }
 }
